dab5f4b402661ccb4808583957fdbd733bf8b9c3
Lukas Kubanek <lukas.kubanek@me.com>
2014-10-25 16:59:52 +0200

    Improved project structure

---
+++ Sources/AppDelegate.swift
@@ -0,0 +1,13 @@
+//
+//  AppDelegate.swift
+//  DetachablePopover
+//
+//  Created by Lukas Kubanek on 25.10.2014.
+//  Copyright (c) 2014 Lukas Kubanek. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain class AppDelegate: NSObject, NSApplicationDelegate {
+
+}
---
+++ Sources/ViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ViewController.swift
+//  DetachablePopover
+//
+//  Created by Lukas Kubanek on 25.10.2014.
+//  Copyright (c) 2014 Lukas Kubanek. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+}
